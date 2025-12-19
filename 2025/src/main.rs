bb76e2ae73aa5442b56abd996dec61e4e045fc98
jannik-reaZn <jannik.harm@gmail.com>
2025-12-19 17:46:14 +0100

    Added main

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -1,45 +1,10 @@
-const DIAL_MAX_NUMBER_OF_POSITIONS: i32 = 99;
+// This file is no longer used. 
+// Run individual days with: cargo run --bin day01
+// Or use the binaries in src/bin/
 
 fn main() {
-  let rotations = [("L", 20)];
-  find_out_password(&rotations);
+    println!("Please run individual days using:");
+    println!("  cargo run --bin day01");
+    println!("  cargo run --bin day02");
+    println!("  etc.");
 }
-
-/// Calulates password for the dial.
-/// This is done by counting how often the dial points to 0.
-fn find_out_password(rotations: &[(&str, i32)]) -> u32 {
-  let mut password: u32 = 0;
-  // The current position always starts at 50
-  let mut current_position: i32 = 50;
-
-  for rotation in rotations {
-    current_position = apply_rotation_to_dial(current_position, &rotation);
-    if current_position == 0 {
-      password += 1
-    }
-  }
-
-  password
-}
-
-fn apply_rotation_to_dial(current_position: i32, rotation: &(&str, i32)) -> i32 {
-  let mut rotation_number: i32 = rotation.1;
-  if rotation.0 == "L" {
-    rotation_number = rotation.1 * -1;
-  }
-
-  let applied_rotation: i32 = current_position + rotation_number;
-  let mut _final_position: i32 = applied_rotation;
-
-  // NOTE: The assumption is that the number of rotations is between 0-99
-  if applied_rotation < 0 {
-    _final_position = DIAL_MAX_NUMBER_OF_POSITIONS + 1 + applied_rotation;
-  } else if applied_rotation >= DIAL_MAX_NUMBER_OF_POSITIONS {
-    _final_position = applied_rotation - DIAL_MAX_NUMBER_OF_POSITIONS - 1;
-  } 
-
-  return _final_position;
-}
-
-#[cfg(test)]
-mod tests;
