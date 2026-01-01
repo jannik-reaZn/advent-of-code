431fa829c9b66f9cc38f75fdf33e5ea0fcc52944
jannik-reaZn <jannik.harm@gmail.com>
2026-01-01 13:55:07 +0100

    Implemented first part of day02

--- 2025/src/days/day02.rs
+++ 2025/src/days/day02.rs
@@ -3,18 +3,55 @@
 /// This module contains the logic for solving both parts of day 2.
 /// Tests are included in this file using #[cfg(test)].
 
-pub fn part1(input: &str) -> i32 {
-    // TODO: Implement part 1
-    0
+pub fn part1(input: &str) -> i64 {
+    let mut result = 0;
+    let splitted_sequences = split_sequences(input);
+    for seq in splitted_sequences {
+        let start = seq.0;
+        let end = seq.1;
+        for number in start..=end {
+            let number_str = number.to_string();
+            if is_sequence_part_1(&number_str) {
+                result += number;
+            }
+        }
+    }
+    result
 }
 
-pub fn part2(input: &str) -> i32 {
-    // TODO: Implement part 2
-    0
+pub fn part2(input: &str) -> i64 {
+    let mut result = 0;
+    let splitted_sequences = split_sequences(input);
+    for seq in splitted_sequences {
+        let start = seq.0;
+        let end = seq.1;
+        for number in start..=end {
+            let number_str = number.to_string();
+            if is_sequence_part_2(&number_str) {
+                result += number;
+            }
+        }
+    }
+    result
 }
 
-/// Cecks if a given string is made only of some sequence of digits repeated twice
-pub fn is_sequence(seq: &str) -> bool {
+/// Splits the input string into a vector of tuples representing the start and end of each sequence.
+pub fn split_sequences(input: &str) -> Vec<(i64, i64)> {
+    let mut splitted_sequences: Vec<(i64, i64)> = Vec::new();
+    let sequence_ranges = input.split(",");
+    for sequences in sequence_ranges {
+        let sequence = sequences.split("-");
+        let collection = sequence.collect::<Vec<&str>>();
+        splitted_sequences.push((
+            collection[0].parse::<i64>().unwrap(),
+            collection[1].parse::<i64>().unwrap(),
+        ));
+    }
+    splitted_sequences
+}
+
+/// Checks if a given string is made only of some sequence of digits repeated twice
+pub fn is_sequence_part_1(seq: &str) -> bool {
     let seq_size = seq.len();
 
     if seq_size < 2 {
@@ -31,6 +68,40 @@ pub fn is_sequence(seq: &str) -> bool {
     false
 }
 
+pub fn is_sequence_part_2(seq: &str) -> bool {
+    let mut window_size = 0;
+
+    let seq_size = seq.len();
+
+    if seq_size < 2 {
+        return false;
+    }
+
+    let i: usize = 0;
+    let j: usize = i + window_size;
+    let k: usize = i + window_size + 1;
+    let l: usize = k + window_size;
+
+    while window_size < seq_size / 2 {
+        for i in i..seq_size / 2 {
+            assert!(i <= j);
+            assert!(j < k);
+            assert!(k <= l);
+            assert!(l < seq_size);
+
+            let first_sub_sequence: &str = &seq[i..j];
+            let second_sub_sequence: &str = &seq[k..l];
+
+            if first_sub_sequence == second_sub_sequence {
+                return true;
+            }
+        }
+        window_size += 1
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,8 +109,8 @@ mod tests {
 
     #[test]
     fn test_part1_example() {
-        let input = "your test input here";
-        assert_eq!(part1(input), 0); // Replace with expected result
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!(part1(input), 1227775554);
     }
 
     #[rstest]
@@ -48,14 +119,38 @@ mod tests {
     #[case("123123", true)]
     #[case("101", false)]
     #[case("5", false)]
-    fn test_is_sequence(#[case] sequence: &str, #[case] expected: bool) {
-        let result = is_sequence(sequence);
+    fn test_is_sequence_part_1(#[case] sequence: &str, #[case] expected: bool) {
+        let result = is_sequence_part_1(sequence);
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
+    #[case("12341234", true)]
+    #[case("123123123", true)]
+    #[case("1212121212", true)]
+    #[case("1111111", true)]
+    #[case("55", true)]
+    #[case("6464", true)]
+    #[case("123123", true)]
+    #[case("101", false)]
+    #[case("5", false)]
+    fn test_is_sequence_part_2(#[case] sequence: &str, #[case] expected: bool) {
+        let result = is_sequence_part_2(sequence);
         assert_eq!(result, expected);
     }
 
     #[test]
     fn test_part2_example() {
-        let input = "your test input here";
-        assert_eq!(part2(input), 0); // Replace with expected result
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!(part2(input), 4174379265);
+    }
+
+    #[test]
+    fn test_split_sequences() {
+        let input = "11-22,95-115";
+        let splitted_sequence = split_sequences(input);
+        assert!(splitted_sequence.len() == 2);
+        assert!(splitted_sequence[0] == (11, 22));
+        assert!(splitted_sequence[1] == (95, 115));
     }
 }
