6208f62ed22173f855adf2c4816da5540080d96c
jannik-reaZn <jannik.harm@gmail.com>
2026-02-15 11:42:44 +0100

    Implemented helper function with base test

--- 2025/src/days/day04.rs
+++ 2025/src/days/day04.rs
@@ -1,5 +1,5 @@
 /// Solution for Day 4
-/// 
+///
 /// This module contains the logic for solving both parts of day 4.
 /// Tests are included in this file using #[cfg(test)].
 
@@ -13,9 +13,23 @@ pub fn part2(input: &str) -> i32 {
     0
 }
 
+pub fn get_nof_adjecent_neighbours(matrix: [[char; 3]; 3]) -> i32 {
+    6
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case([['.', '.', '@'], ['@', '@', '@'], ['@', '@', '@']], 6)]
+    fn test_get_nof_adjecent_neighbours(
+        #[case] matrix: [[char; 3]; 3],
+        #[case] expected_neighbours: i32,
+    ) {
+        assert_eq!(get_nof_adjecent_neighbours(matrix), expected_neighbours);
+    }
 
     #[test]
     fn test_part1_example() {
