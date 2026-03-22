f2046e422e65a4ec964a111d09717f75b0e6cd6c
jannik-reaZn <jannik.harm@gmail.com>
2026-03-22 15:14:49 +0100

    Refactored code

--- 2025/src/days/day05.rs
+++ 2025/src/days/day05.rs
@@ -2,10 +2,7 @@
 ///
 /// This module contains the logic for solving both parts of day 5.
 /// Tests are included in this file using #[cfg(test)].
-use std::{
-    cmp::{max, min},
-    collections::HashSet,
-};
+use std::{cmp::max, collections::HashSet};
 
 pub fn part1(input: &str) -> i64 {
     let mut amount_fresh_ingredients = 0;
@@ -36,7 +33,7 @@ pub fn part2(input: &str) -> i64 {
 
     let mut number_of_fresh_ingredients = 0;
     for ingredient in ingredients {
-        number_of_fresh_ingredients += ingredient.1 - ingredient.0;
+        number_of_fresh_ingredients += ingredient.1 - ingredient.0 + 1;
     }
 
     number_of_fresh_ingredients
@@ -81,61 +78,58 @@ pub fn get_ingredients_from_range(ingredient_range: &Vec<(i64, i64)>) -> HashSet
     ingredient_in_ranges
 }
 
-// trait Stack {
-//     fn top(&self) -> Option<&f64>;
-// }
+trait Stack<T> {
+    fn push(&mut self, item: T);
+    fn top(&self) -> Option<&T>;
+    fn size(&self) -> usize;
+}
 
-// impl Stack for Vec<f64> {
-//     fn top(&self) -> Option<&f64> {
-//         match &self[..] {
-//             [] => None,
-//             [.., n] => Some(n),
-//         }
-//     }
-// }
+impl<T> Stack<T> for Vec<T> {
+    fn push(&mut self, item: T) {
+        self.push(item);
+    }
+
+    fn top(&self) -> Option<&T> {
+        self.last()
+    }
+
+    fn size(&self) -> usize {
+        self.len()
+    }
+}
 
 /*
 Algorithm:
-1. If no ingredients, return empty vec
-2. Sort vec of tuples. First check first element, if equal use the second element.
-3. Remove duplicates
-4. Push first tuple onto stack
-5. Iterate over 1..n tuple
-    5.1 Check for overlapping ranges.
-    This is only the case of the first element in tuple is larger than the the first element in the tuple at the top of the stack
-    AND smaller than the second elements of the same tuple
-    5.2 If it is overlapping, the ranges can be merged.
-    The question which second element should be taken (the tuple form the stack or the tuple in the current iteration),
+1. Sort the vec of tuples. First check first element, if equal use the second element.
+2. Remove duplicates.
+3. Push first tuple onto stack.
+4. Iterate over 1..n tuples.
+    4.1 Check for overlapping ranges.
+    This is the case if the first element of the current tuple is smaller than or equal to
+    the second element of the tuple at the top of the stack. Then overlapping ranges can be merged.
+    The question which second element should be taken (the tuple from the stack or the tuple in the current iteration)
     is answered by its maximum value.
-    5.3 If no overlap is present, the tuple is pushed to the stack.
+    4.2 If no overlap is present, the tuple is pushed to the stack.
 */
 pub fn merge_ingredient_ranges(ingredient_range: &Vec<(i64, i64)>) -> Vec<(i64, i64)> {
-    let mut merged_ingredient_ids: Vec<(i64, i64)> = Vec::new();
+    let mut sorted = ingredient_range.clone();
+    sorted.sort();
+    sorted.dedup();
 
-    merged_ingredient_ids.sort();
-    merged_ingredient_ids.dedup();
+    let mut merged: Vec<(i64, i64)> = Vec::new();
+    Stack::push(&mut merged, sorted[0]);
 
-    merged_ingredient_ids.push(ingredient_range[0]);
-
-    for i in 1..ingredient_range.len() {
-        let size = merged_ingredient_ids.len();
-        // Check for overlap
-        if merged_ingredient_ids[size - 1].0 <= ingredient_range[i].0
-            && ingredient_range[i].0 <= merged_ingredient_ids[size - 1].1
-        {
-            // Merge
-            merged_ingredient_ids[size - 1].1 =
-                max(merged_ingredient_ids[size - 1].1, ingredient_range[i].1)
-        } else if merged_ingredient_ids[size - 1].0 <= ingredient_range[i].1
-            && ingredient_range[i].1 <= merged_ingredient_ids[size - 1].1
-        {
-            merged_ingredient_ids[size - 1].0 =
-                min(merged_ingredient_ids[size - 1].0, ingredient_range[i].0)
+    for i in 1..sorted.len() {
+        // Check for overlap: current start falls within or adjacent to top of stack
+        if sorted[i].0 <= merged.top().unwrap().1 {
+            // Merge by extending the end if needed
+            let last = merged.size() - 1;
+            merged[last].1 = max(merged[last].1, sorted[i].1);
         } else {
             // No overlap
-            merged_ingredient_ids.push(ingredient_range[i])
+            Stack::push(&mut merged, sorted[i]);
         }
     }
 
-    return merged_ingredient_ids;
+    merged
 }
